660eb72c28e3d35e331c5c0fcaa32e98ca0cacb4
Shane Ryan <onyx.shane@gmail.com>
2018-03-02 08:47:34 -0800

    add initial component text

---
+++ a-posture-fix/src/comonents/Examples.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Examples(){
+  return();
+}
+
+export default Examples;
---
+++ a-posture-fix/src/comonents/Presets.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Presets(){
+  return();
+}
+
+export default Presets;
